2e2292819afa8f7f6599fd7f3d8416b04cfe30b8
ahmadmm01 <skyahmad74@gmail.com>
2022-03-30 15:22:14 +0700

    Update properties Form1 to EmployeeDetails

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace RetrievingData
 {
-    partial class Form1
+    partial class EmployeeDetails
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace RetrievingData
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EmployeeDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "EmployeeDetails";
+            this.Text = "EmployeeDetails";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RetrievingData
 {
-    public partial class Form1 : Form
+    public partial class EmployeeDetails : Form
     {
-        public Form1()
+        public EmployeeDetails()
         {
             InitializeComponent();
         }
